8aeeb4c0f5cc8d92a3f2549043804d267b63cfda
SeminKiim <seminik62@gmail.com>
2023-10-25 19:22:13 +0900

    vue-pdf-app 설치했는데 에러뜸
    구글드라이브 링크 거는게 나을것 같다.

---
+++ src/views/GreenEarth.vue
@@ -0,0 +1,49 @@
+<template>
+    <header>
+        <div @click="movePage('/')">Hey there!</div>
+    </header>
+    <div class="description">
+        <div class="title">'Green Earth'웹사이트는</div>
+        <div>도농상생프로젝트의 일환으로,<br>아동이 있는 가족을 대상으로 한 김장 체험 프로그램 입니다.</div>
+    </div>
+    <div class="pdf-wrapper">
+        <vue-pdf-app></vue-pdf-app>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        movePage(path) {
+            this.$router.push(path)
+        }
+    }
+}
+</script>
+
+<style lang="less">
+header {
+    color: #6b4d38;
+    background-color: #f7f2f0;
+    padding-left: 10%;
+    font-size: 2.5em;
+    font-weight: bold;
+}
+
+
+.description {
+    font-size: 2rem;
+    padding: 10%;
+    color: #6b4d38;
+    background-color: #f7f2f0;
+    height: 100vh;
+    font-size: 36px;
+
+    .title {
+        font-size: 2em;
+        font-weight: bold;
+        height: 30vh;
+        text-align: center;
+    }
+}
+</style>
--- src/views/Kimjangtalk.vue
+++ src/views/Kimjangtalk.vue
@@ -2,41 +2,61 @@
     <header>
         <div @click="movePage('/')">Hey there!</div>
     </header>
-    <div class="intro">
-        <div class="header">'김장톡 프로젝트는'</div>
-        <div>도농상생프로젝트의 일환으로, 아동이 있는 가족을 대상으로 한 김장 체험 프로그램 입니다.</div>
-        <v-img src="../assets/kimjangtalk.png" class="thumbnail"></v-img>
-        <div>이미지를 클릭해보세요!</div>
+    <div class="description">
+        <div class="title">'김장톡 프로젝트'는</div>
+        <div>도농상생프로젝트의 일환으로,<br>아동이 있는 가족을 대상으로 한 김장 체험 프로그램 입니다.</div>
+    </div>
+    <div class="pdf-wrapper">
+        <vue-pdf-app pdf="../assets/kimjangtalk.pdf"></vue-pdf-app>
     </div>
 </template>
 
 <script>
+//vue-pdf
+import pdf from 'vue-pdf'
+
+//vue-pdf-app
+import VuePdfApp from "vue-pdf-app"
+
 export default {
+    components: {
+        pdf,
+        VuePdfApp
+    },
     methods: {
         movePage(path) {
             this.$router.push(path)
-        }
+        }, openHandler(pdfApp) {
+            window._pdfApp = pdfApp;
+        },
     }
 }
 </script>
 
 <style lang="less">
+@import "https://unpkg.com/vue-pdf-app@2.0.0/dist/icons/main.css";
+
 header {
     color: #6b4d38;
     background-color: #f7f2f0;
-    text-align: center;
+    padding-left: 10%;
     font-size: 2.5em;
     font-weight: bold;
 }
 
-.intro {
+.description {
+    font-size: 2rem;
     padding: 10%;
     color: #6b4d38;
-    font-size: medium;
     background-color: #f7f2f0;
+    height: 100vh;
+    font-size: 36px;
 
-    .header {
-        font-size: larger;
+    .title {
+        font-size: 2em;
+        font-weight: bold;
+        height: 30vh;
+        text-align: center;
     }
 }
 </style>
